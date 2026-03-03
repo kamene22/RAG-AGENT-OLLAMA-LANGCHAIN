193693bdb96a2c5950714c4f145e230d6469ad37
kamene22 <kamenemonica19@gmail.com>
2026-03-03 15:26:26 +0300

    Update RAG agent and vector store scripts

--- main.py
+++ main.py
@@ -1,18 +1,48 @@
-from langchain_ollama import OllamaLLM
+from langchain_ollama import ChatOllama
 from langchain_core.prompts import ChatPromptTemplate
+from vector import retriever
 
-model = OllamaLLM(model = 'llama3.2')
+model = ChatOllama(model="llama3.2:3b")
 
 template = """
-You are an epert in answering questions about a pizza restaurant
+You are an expert in answering questions about a pizza restaurant.
 
-Here are some relevant: {reviews}
+Here are some relevant reviews:
+{reviews}
 
-Here is the question to answer: {question}
+Question: {question}
+
+Answer based only on the reviews above.
 """
 
 prompt = ChatPromptTemplate.from_template(template)
+
 chain = prompt | model
 
-result = chain.invoke({"reviews": [] ,"question":"what is the best pizza place in town"})
-print(result)
+
+while True:
+
+    print("\n....................")
+
+    question = input("Ask your question (q to quit): ")
+
+    if question.lower() == "q":
+        break
+
+
+    # retrieve documents
+    docs = retriever.invoke(question)
+
+    # convert docs to text
+    reviews_text = "\n\n".join(
+        doc.page_content for doc in docs
+    )
+
+    # run LLM
+    result = chain.invoke({
+        "reviews": reviews_text,
+        "question": question
+    })
+
+    print("\nAnswer:")
+    print(result.content)
